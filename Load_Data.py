c440a6113a41f12013ef2f7b1afb9e010773c319
Your Name <you@example.com>
2025-07-01 00:43:15 +1000

    initial Comit: Data Loading

---
+++ Load_Data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+# Providing the full path 
+data = pd.read_csv("C:/Users/ankit/Documents/AAAAA SHIWANI/New Project/Python + Power BI/shopping_trends.csv")
+   
+ # Show first 5 rows  
+print(data.head())
+  
